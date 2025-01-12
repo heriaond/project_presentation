96a22cd3c377998865eedc97cdbe0047f2756f65
Petr Herian <82937545+herianp@users.noreply.github.com>
2025-01-12 21:39:40 +0100

    Merge task-endpoint into master (#2)
    * init endpoints with body and empty
    
    * endpoints fix

---
+++ apps/backend/Infrastructure/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using Infrastructure.Models;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Infrastructure.Controllers;
+
+[ApiController]
+[Route("api/weather")]
+public class StudentController : ControllerBase
+{
+    private readonly IStudentService studentService;
+    public StudentController(IStudentService studentService)
+    {
+        this.studentService = studentService;
+    }
+    
+    [HttpGet("getNumber")]
+    public int GetNumber()
+    {
+        return studentService.SumTwoNumbers(1,2);
+    }
+    
+    [HttpPost("getNumberSum")]
+    public int GetNumberSum([FromBody] RequestNumbers requestNumbers)
+    {
+        var a = requestNumbers.Anum;
+        var b = requestNumbers.Bnum;
+        return studentService.SumTwoNumbers(a,b);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ apps/backend/Infrastructure/Services/StudentService.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.Services;
+
+public class StudentService : IStudentService
+{
+    public int SumTwoNumbers(int a, int b)
+    {
+        return a+b;
+    }
+}
