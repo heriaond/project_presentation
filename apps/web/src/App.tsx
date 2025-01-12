131bca887df6e10a3d6d99efedeb58b20a88116c
Ondrej Herian <ondra.heri@gmail.com>
2025-01-12 21:36:00 +0100

    feat: project config updated

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from '@/assets/react.svg';
+import viteLogo from '/vite.svg';
+import heriLogo from '@packages/images/logo_book.png';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -14,6 +15,9 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={heriLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React + Petis is god</h1>
@@ -29,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
